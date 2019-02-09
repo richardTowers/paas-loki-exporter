e359716933efd5ad52f885294af5e5de8663ad80
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-02-09 15:51:17 +0000

    Implement minimal viable exporter
    Reads from the firehose, writes to promtail.
    
    One thing that's slightly annoying is that Loki wants the logs to arrive
    in order. Since the logs that come out of the firehose are not ordered
    we're pushing them into Loki with the timestamp set as `time.Now()` as
    the exporter sees it.
    
    This isn't great, because:
    
    1) The timestamps on the logs won't be the time they were actually
    raised
    2) We can't run multiple exporters writing to the same Loki, as they'll
    end up racing each other
    
    We might be able to resolve the first issue by doing some buffering.

--- main.go
+++ main.go
@@ -2,62 +2,85 @@ package main
 
 import (
 	"crypto/tls"
-	"flag"
-	"fmt"
+	"log"
+	"net/url"
 	"os"
+	"time"
 
 	"github.com/cloudfoundry/noaa/consumer"
 	"github.com/cloudfoundry/sonde-go/events"
+	"github.com/cortexproject/cortex/pkg/util/flagext"
+	kitlog "github.com/go-kit/kit/log"
 	promtailclient "github.com/grafana/loki/pkg/promtail/client"
+	"github.com/prometheus/common/model"
 )
 
-const firehoseSubscriptionId = "firehose-a"
-
-var (
-	dopplerAddress = os.Getenv("DOPPLER_ADDR")
-	authToken      = os.Getenv("CF_ACCESS_TOKEN")
-)
+const firehoseSubscriptionId = "paas-loki-exporter"
 
 func main() {
-	filterType := flag.String("filter", "all", "filter messages by 'logs' or 'metrics' (default: all)")
-	flag.Parse()
+	dopplerAddress, authToken, parsedLokiUrl := readEnvironment()
+	logger := kitlog.NewLogfmtLogger(kitlog.NewSyncWriter(os.Stderr))
+	promtailClient := createPromtailClient(logger, parsedLokiUrl)
 
-	cnsmr := consumer.New(dopplerAddress, &tls.Config{InsecureSkipVerify: true}, nil)
-	cnsmr.SetDebugPrinter(ConsoleDebugPrinter{})
+	firehoseConsumer := consumer.New(dopplerAddress, &tls.Config{}, nil)
+	msgChan, errorChan := firehoseConsumer.FilteredFirehose(firehoseSubscriptionId, authToken, consumer.LogMessages)
 
-	fmt.Println("===== Streaming Firehose (will only succeed if you have admin credentials)")
+	go readErrorChan(logger, errorChan)
+	forwardMsgChanToPromtail(promtailClient, logger, msgChan)
+}
 
-	promtailclient.New(promtailclient.Config{}, nil)
-
-	var (
-		msgChan   <-chan *events.Envelope
-		errorChan <-chan error
-	)
-
-	switch *filterType {
-	case "logs":
-		msgChan, errorChan = cnsmr.FilteredFirehose(firehoseSubscriptionId, authToken, consumer.LogMessages)
-	case "metrics":
-		msgChan, errorChan = cnsmr.FilteredFirehose(firehoseSubscriptionId, authToken, consumer.Metrics)
-	default:
-		msgChan, errorChan = cnsmr.Firehose(firehoseSubscriptionId, authToken)
+func readEnvironment() (dopplerAddress string, authToken string, parsedLokiUrl *url.URL) {
+	dopplerAddress, ok := os.LookupEnv("DOPPLER_ADDR")
+	if !ok {
+		log.Fatalf("variable DOPPLER_ADDR must be set")
 	}
+	authToken, ok = os.LookupEnv("CF_ACCESS_TOKEN")
+	if !ok {
+		log.Fatalf("variable CF_ACCESS_TOKEN must be set")
+	}
+	lokiUrl, ok := os.LookupEnv("LOKI_URL")
+	if !ok {
+		log.Fatalf("variable LOKI_URL must be set")
+	}
+	parsedLokiUrl, err := url.Parse(lokiUrl)
+	if err != nil {
+		log.Fatalf("could not parse loki URL (%s) as a URL: %v", lokiUrl, err)
+	}
+	return dopplerAddress, authToken, parsedLokiUrl
+}
 
-	go func() {
-		for err := range errorChan {
-			fmt.Fprintf(os.Stderr, "%v\n", err.Error())
-		}
-	}()
+func createPromtailClient(logger kitlog.Logger, parsedLokiUrl *url.URL) *promtailclient.Client {
+	promtailConfig := promtailclient.Config{
+		URL: flagext.URLValue{
+			URL: parsedLokiUrl,
+		},
+	}
+	promtailClient, err := promtailclient.New(promtailConfig, logger)
+	if err != nil {
+		log.Fatalf("could not create promtail client: %v", err)
+	}
+	return promtailClient
+}
 
+func readErrorChan(logger kitlog.Logger, errorChan <-chan error) {
+	for err := range errorChan {
+		_ = logger.Log("level", "error", "message", err.Error())
+	}
+}
+
+func forwardMsgChanToPromtail(promtailClient *promtailclient.Client, logger kitlog.Logger, msgChan <-chan *events.Envelope) {
 	for msg := range msgChan {
-		fmt.Printf("%v \n", msg)
+		logMessage := msg.LogMessage
+		labelSet := model.LabelSet{
+			"app":          model.LabelValue(logMessage.GetAppId()),
+			"source_type":  model.LabelValue(logMessage.GetSourceType()),
+			"source":       model.LabelValue(logMessage.GetSourceInstance()),
+			"message_type": model.LabelValue(logMessage.GetMessageType().String()),
+		}
+		promtailError := promtailClient.Handle(labelSet, time.Now(), string(logMessage.Message))
+		if promtailError != nil {
+			_ = logger.Log("level", "error", "message", promtailError.Error())
+		}
+		print(".")
 	}
 }
-
-type ConsoleDebugPrinter struct{}
-
-func (c ConsoleDebugPrinter) Print(title, dump string) {
-	println(title)
-	println(dump)
-}
-
